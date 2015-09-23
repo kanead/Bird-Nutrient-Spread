b9c3ca7b6d33c9c859111661940596c7582732f9
Adam Kane <kanead@tcd.ie>
2015-09-23 16:39:20 +0100

    Code to loop model runs

--- RNetLogo.R
+++ RNetLogo.R
@@ -89,6 +89,20 @@ y <- nutrientcoords$ycor
 length(x)
 length(y)
 
+##-----------------------------------------------------------------------
+## Alternative way to run model based on number of days 
+##-----------------------------------------------------------------------
+
+NLCommand("setup")
+NLDoCommandWhile("day < 10",  "go")
+nutrientcoords <- list()
+nutrientcoords[[i]] <- NLGetAgentSet(c("who","xcor","ycor"), "nutrients")
+nutrientcoords[[i]]
+nutrientcoords<-as.data.frame(nutrientcoords[[nruns]]) 
+
+##---------------------------------------------------------------------
+## Calculate mean nearest neighbour distance
+##---------------------------------------------------------------------
 distance <- function(x1,y1,x2,y2) sqrt((x2-x1)^2 + (y2-y1)^2)
 r<-numeric(length(x))
 nn<-numeric(length(x))
@@ -102,8 +116,11 @@ nn[i]<-which(d==min(d[-i]))
 plot(x, y, pch = 16)
 for (i in 1:length(x)) lines(c(x[i],x[nn[i]]),c(y[i],y[nn[i]]))
 
-mean(r)
-#Quadrat-based methods
+mean(r) ## this calculates mean nearest neighbour distance
+
+##---------------------------------------------------------------------
+## Quadrat-based methods
+##---------------------------------------------------------------------
 grid(50,50,lty=1)
 
 xt<-cut(x,seq(-25,25,1))
@@ -111,3 +128,62 @@ yt<-cut(y,seq(-25,25,1))
 
 count<-as.vector(table(xt,yt))
 table(count)
+
+##-----------------------------------------------------------------------
+## WORKING MODEL
+##-----------------------------------------------------------------------
+library(RNetLogo)
+library(spatstat)
+NLStart("C:/Program Files/NetLogo 5.0")
+## specifying the nl.version=5 throws an error so I've deleted this
+nl.path <- "C:/Program Files/NetLogo 5.0"
+
+model.path <- "/models/Sample Models/Biology/nutrient transfer model diffusion.nlogo"
+NLLoadModel(paste(nl.path,model.path,sep=""))
+
+# set numbers and parameters 
+NLCommand("set N-vultures 0")
+NLCommand("set N-carcasses 10")
+NLCommand("set N-hyenas 20")
+NLCommand("set v-hyena 0.1")
+
+# setup model
+NLCommand("setup")
+
+
+NLCommand("setup")
+NLDoCommandWhile("day < 1",  "go")
+nutrientcoords <- list()
+nutrientcoords <- NLGetAgentSet(c("who","xcor","ycor"), "nutrients")
+nutrientcoords
+nutrientcoords<-as.data.frame(nutrientcoords) 
+
+x <- nutrientcoords$xcor
+y <- nutrientcoords$ycor
+
+## nearest neighbour distance function from package spatstat
+dist <- nndist(x,y) 
+mean(dist) ## mean nn dist
+
+plot(x, y, pch = 16)
+for (i in 1:length(x)) lines(c(x[i],x[nn[i]]),c(y[i],y[nn[i]]))
+
+## the above is essentially this function from Crawley
+# distance <- function(x1,y1,x2,y2) sqrt((x2-x1)^2 + (y2-y1)^2)
+# r<-numeric(length(x))
+# nn<-numeric(length(x))
+# d<-numeric(length(x))
+# for (i in 1:length(x)) {
+# for (k in 1:length(x)) d[k]<-distance(x[i],y[i],x[k],y[k])
+# r[i]<-min(d[-i])
+# nn[i]<-which(d==min(d[-i]))
+# }
+## this calculates mean nearest neighbour distance
+# mean(r)
+
+
+
+
+
+
+
